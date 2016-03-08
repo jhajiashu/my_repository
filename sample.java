a211c57db5dc8e4046e5213d435465f7edd2e6f7
jhajiashu <ashu.kr18@gmail.com>
2016-03-08 15:55:12 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,10 @@
 
-class firsttime {
-private static void main(String[] args) {
- System.out.println("hello world");
-}
+class firsttime 
+{
+	private static void main(String[] args) 
+	{
+ 	System.out.println("hello world");
+ 	System.outprintln("edited this file directly from github website!");
+	}
 }
 
